13e424cfbf7a70faf04bf27ec556eb599710a7eb
nichelia <nich.elia@gmail.com>
2021-07-02 00:30:48 +0100

    Task client now sends create/update requests to the task service

--- clearly-client-task/app/main.py
+++ clearly-client-task/app/main.py
@@ -1,10 +1,10 @@
 import asyncio
-import httpx
 import json
+
+import httpx
 from clearly.client import ClearlyClient
 from clearly.protos.clearly_pb2 import CaptureRequest
-from google.protobuf.json_format import MessageToDict
-from google.protobuf.json_format import MessageToJson
+from google.protobuf.json_format import MessageToDict, MessageToJson
 
 from app._version import __version__
 from app.logger import LOGGER, setup_logger
@@ -14,16 +14,20 @@ from app.settings import settings
 
 def post_to_task_service(client, data):
     LOGGER.debug(f"Posting to {settings.TASK_SERVICE_URI} with data {data}")
-    r = client.post(settings.TASK_SERVICE_URI, data=data)
+    r = client.post(settings.TASK_SERVICE_URI, data=json.dumps(data))
+    LOGGER.debug(f"Request: {r}")
+
+
+def put_to_task_service(client, data):
+    LOGGER.debug(f"Posting to {settings.TASK_SERVICE_URI} with data {data}")
+    task_uuid = data.pop("uuid", None)
+    uri = f"{settings.TASK_SERVICE_URI}{task_uuid}"
+    r = client.put(uri, data=json.dumps(data))
     LOGGER.debug(f"Request: {r}")
 
 
 def parse_task_data(data):
     task_data = MessageToDict(data)
-    task_data.pop("timestamp", None)
-    task_data.pop("state", None)
-    task_data["args"] = []
-    task_data["kwargs"] = {}
     return task_data
 
 
@@ -49,7 +53,11 @@ def init():
                 if realtime.task.state == "RECEIVED":
                     LOGGER.debug(f"New Task detected with state: {realtime.task.state} and uuid: {realtime.task.uuid}")
                     task_data = parse_task_data(realtime.task)
-                    post_to_task_service(http_client, json.dumps(task_data))
+                    post_to_task_service(http_client, task_data)
+                else:
+                    LOGGER.debug(f"Task update detected with state: {realtime.task.state} and uuid: {realtime.task.uuid}")
+                    task_data = parse_task_data(realtime.task)
+                    put_to_task_service(http_client, task_data)
     except KeyboardInterrupt:  # pragma: no cover
         pass
 
